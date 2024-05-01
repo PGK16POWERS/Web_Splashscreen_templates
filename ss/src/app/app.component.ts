50138ba3a903c5bc217efcd81ed898560d55074c
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-05-01 11:13:00 +0200

    styles

--- ss/src/app/app.component.ts
+++ ss/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ss';
+
+  ngOnInit(): void {
+      const splashBg = document.querySelector("#splash-bg") as HTMLElement;
+
+      setTimeout(() => {
+        splashBg.classList.add("boltAnimation");
+    }, 3000); // 3000 milliseconds = 3 seconds
+  }
 }
